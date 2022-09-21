90639aec46724620ed6a521e4bfe7e8385888554
THE GREAT ONE <iim2godly@gmail.com>
2022-09-21 10:05:40 -0400

    It works!

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-let quoteList = [ ['If you don\'t like something, change it. If you can\'t change it, change your attitude.', 'Maya Angelou'],  ['One of the lessons that I grew up with was to always true to yourself and never let what somebody else says distract you from your goals.', 'Michelle Obama'], ['Be thankful for what you have; you\'ll end up having more. If you concentrate on what you don\'t have, you will never ever have enough.' , 'Oprah Winfrey'], ['Keep your face always toward the sunshine-and shadows will fall behind you.', 'Walt Whitman'], ['If you don\'t like the road you\'re walking, start paving another one.' , 'Dolly Parton'] ['I learned a long time ago that there is something worse than missing the goal, and that\'s not pulling the trigger.', 'Mia Hamm'] , ['Success is stumbling from failure to failure with no loss of enthusiam.', 'Winston Churchill'] , ['I never lose. Either I win or learn.','Nelson Mendela'] ];
+let quoteList = [ ['If you don\'t like something, change it. If you can\'t change it, change your attitude. ', ' -Maya Angelou'],  ['One of the lessons that I grew up with was to always true to yourself and never let what somebody else says distract you from your goals.', ' -Michelle Obama'], ['Be thankful for what you have; you\'ll end up having more. If you concentrate on what you don\'t have, you will never ever have enough.' , ' -Oprah Winfrey'], ['Keep your face always toward the sunshine-and shadows will fall behind you.', ' -Walt Whitman'], ['If you don\'t like the road you\'re walking, start paving another one.' , ' -Dolly Parton'] ['I learned a long time ago that there is something worse than missing the goal, and that\'s not pulling the trigger.', ' -Mia Hamm'] , ['Success is stumbling from failure to failure with no loss of enthusiam.', ' -Winston Churchill'] , ['I never lose. Either I win or learn.',' -Nelson Mendela']];
 
-//console.log(quoteList[0,1]); 
+const motivate = (y) => {
+    let x = quoteList;
+    y=(Math.floor(Math.random()*x.length));
+     return console.log(x[y]);
+   }   
 
-
-const motivate = (quote, name) => {
-   let x = quoteList[]
-   let quote = quote
-   let name =
-
-  return  console.log(x)
-   }    g
-
-console.log(motivate());asdfasdfasdf
+(motivate()); 
